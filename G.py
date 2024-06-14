48198d5abce0ebb253dea12f3c8f7a096028fd6a
Leiner2024 <136385391+Leiner2024@users.noreply.github.com>
2024-06-14 10:04:24 +0200

    Update G.py

--- G.py
+++ G.py
@@ -1,14 +1,21 @@
 from flask import Flask
 
+# Crea una instancia de la clase Flask para la aplicación web.
 app = Flask(__name__)
 
+# Define una ruta para la URL raíz ('/') de la aplicación.
 @app.route('/')
 def home():
+    # Devuelve una respuesta simple cuando se accede a la URL raíz.
     return "¿Qué onda?"
 
 def main():
+    # Imprime un mensaje indicando que el módulo G se está ejecutando.
     print("Se está ejecutando el módulo G")
+    # Ejecuta la aplicación Flask en el servidor local.
     app.run()
 
+# Verifica si el archivo se está ejecutando como el programa principal.
 if __name__ == "__main__":
+    # Llama a la función main para iniciar la aplicación.
     main()
